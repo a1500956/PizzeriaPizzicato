1ca399fa223f9d5fb0ec5a63a7ccd7290d55d7d7
a1500850 <a1500850@PAO5003Z23.haagahelia.amk>
2016-02-29 14:42:19 +0200

    DAO.java MUISTAKAA LUODA ACCOUNTS.java model.dao ALLE

---
+++ Pizzeria_Pizzicato/src/pizzeria_pizzicato/model/dao/DataAccessObject.java
@@ -0,0 +1,54 @@
+package pizzeria_pizzicato.model.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DataAccessObject {
+
+	protected static Connection getConnection() {
+		Connection connection = null;
+
+		String username = Accounts.DBUSERNAME;
+		String password = Accounts.DBPASSWORD;
+		String url = "jdbc:mysql://localhost:3306/projekti";
+
+		
+		try {
+			
+			Class.forName("org.mariadb.jdbc.Driver").newInstance();
+		
+		
+			connection = DriverManager.getConnection(url, username, password);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return connection;
+	}
+	
+
+	protected static void close(Statement stmt, Connection connection) {
+		close (null, stmt, connection);
+	}
+
+	
+	protected static void close(ResultSet rs, Statement stmt, Connection conn ) {
+		
+		try {
+			if (rs != null) { 
+				rs.close();
+			}
+			if (stmt != null) { 
+				stmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+			
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+}
