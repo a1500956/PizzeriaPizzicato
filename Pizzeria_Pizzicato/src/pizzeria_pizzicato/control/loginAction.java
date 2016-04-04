7f0e8f4744e99e6bc5c2a0f27ab489fcd8888a78
a1500850 <a1500850@PAO5003Z23.haagahelia.amk>
2016-04-04 12:37:19 +0300

    ---

---
+++ Pizzeria_Pizzicato/src/pizzeria_pizzicato/control/loginAction.java
@@ -0,0 +1,30 @@
+package pizzeria_pizzicato.control;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import pizzeria_pizzicato.model.Pizza;
+import pizzeria_pizzicato.model.dao.PizzaDAO;
+
+
+Servlet("/loginAction")
+public class loginAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
